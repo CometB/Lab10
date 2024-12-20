3b051d4f6b1cf471ea28434d648813e29386ad87
CometB <126880018+CometB@users.noreply.github.com>
2024-12-20 13:33:15 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    
+    string data;
+    int n = 0;
+    double mean = 0, sd = 0;
+    
+    while(getline(source, data)){
+        double d = stod(data);
+        mean += d;
+        sd += (d*d);
+        n++;
+    }
+    source.close();
+    
+    mean /= n;
+    sd = sqrt(sd/n - mean*mean);
+    
+    
+    cout << "Number of data = " << n << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << mean << '\n'; 
+    cout << "Standard deviation = " << sd << '\n';
+}
